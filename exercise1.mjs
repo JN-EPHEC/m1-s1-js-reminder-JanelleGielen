caef23a5891aea33614d7c5f975b528168b60901
janellegielen <HE133675@students.ephec.be>
2025-09-23 16:45:44 +0200

    Mess

--- exercise1.mjs
+++ exercise1.mjs
@@ -18,11 +18,32 @@ const users = [
 ];
 
 
-// Etape 1
+// Etape 1 
+//**User Names:** Create a new variable `userNames` that contains an array with only the names of all users.
+    //- _Expected result:_ `['Alice', 'Bob', 'Charlie', 'David', 'Eve']`
+let userNames = [];
 
-// Etape 2
+for (let i = 0; i < users;length; i++) {
+  userNames.push (users [i].name);
+}
+
+console.log(userNames);
+
+// Etape 2 
+//**Engineers:** Create a new variable `engineers` that contains an array of user objects working in the 'Engineering' specialty.
+  //  - _Expected result:_ `[{ id: 2, ... }, { id: 4, ... }]`
+let userEngineers = [];
+
+for (let i = 0; i < users.length; i++){
+  if (users [i]. specialty === "Engineering") {
+    userEngineers.push (user[i]);
+  }
+}
+
+console.log(userEngineers);
 
 // Etape 3
 
 // Etape 4
+
 // Your code here
